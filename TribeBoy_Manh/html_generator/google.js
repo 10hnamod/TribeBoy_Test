273d9c43d9a8784a9607c6bb57996f85ee3f5457
I'm Do Manh <manhdop01@gmail.com>
2022-08-06 22:27:20 +0700

    build loi

--- TribeBoy_Manh/html_generator/google.js
+++ TribeBoy_Manh/html_generator/google.js
@@ -11,9 +11,9 @@
             return /android|Android/i.test(e) ? "android" : /iPad|iPhone|iPod|Macintosh/.test(e) && !window.MSStream ? "iOS" : "android";
 		}
    			
-		var clickTag = "https://play.google.com/store/apps/details?id=com.balance.redball";
+		var clickTag = "https://play.google.com/store/apps/details?id=com.one.m2.world";
    		if (getMobileOS()=="iOS"){
-   			clickTag = "https://apps.apple.com/us/app/cooking-rush-restaurant-game/id1623992858";
+   			clickTag = "https://apps.apple.com/vn/app/id1617245942";
    		}
 
    		window.playsound = false; 
--- TribeBoy_Manh/html_generator/vungle.js
+++ TribeBoy_Manh/html_generator/vungle.js
@@ -3,9 +3,9 @@
 			var e = navigator.userAgent || navigator.vendor || window.opera;
             return /android|Android/i.test(e) ? "android" : /iPad|iPhone|iPod|Macintosh/.test(e) && !window.MSStream ? "iOS" : "android";		 
    			}
-   		var clickTag = "https://play.google.com/store/apps/details?id=com.balance.redball";
+   		var clickTag = "https://play.google.com/store/apps/details?id=com.one.m2.world";
    		if (getMobileOS()=="iOS"){
-   			clickTag = "https://apps.apple.com/us/app/cooking-rush-restaurant-game/id1623992858";
+   			clickTag = "https://apps.apple.com/vn/app/id1617245942";
    		}
    		{/* window.failedIndex = 0;  */}
 		   window.playsound = true;
